bdeda6a92068ef2d35dae1929c67c12a1e663a6a
Tanya Ignatenko <tanigna.work@gmail.com>
2023-03-14 21:06:46 +0700

    Make hero section responsive

--- css-practice/src/Landing/HeroSection/HeroSection.tsx
+++ css-practice/src/Landing/HeroSection/HeroSection.tsx
@@ -2,20 +2,27 @@ import styles from './HeroSection.module.css';
 const HeroSection = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.contentWrapper}>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>
-                        We’ve expanded!
-                        <br/>
-                        Shorten URLs. Generate QR Codes.
-                        <br/>
-                        And now, create Link-in-bios.
-                        <br/>
-                    </h1>
-                    <div className={styles.linksBlock}>
-                        <a className={styles.getStartedLink}>Get Started for Free</a>
-                        <br />
-                        <a className={styles.getQuoteLink}>Get a Quote</a>
+            <img
+                className={styles.mobileHeroImage}
+                src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/08/home-hero-mobile-v2.png"
+                alt=""
+            />
+            <div className={styles.heroContainer}>
+                <div className={styles.contentWrapper}>
+                    <div className={styles.content}>
+                        <h1 className={styles.title}>
+                            We’ve expanded!
+                            <br/>
+                            Shorten URLs. Generate QR Codes.
+                            <br/>
+                            And now, create Link-in-bios.
+                            <br/>
+                        </h1>
+                        <div className={styles.linksBlock}>
+                            <a className={styles.getStartedLink}>Get Started for Free</a>
+                            <br />
+                            <a className={styles.getQuoteLink}>Get a Quote</a>
+                        </div>
                     </div>
                 </div>
             </div>
