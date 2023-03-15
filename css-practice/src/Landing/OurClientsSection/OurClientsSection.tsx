f6712ccb5ffb5433b7b169574a22e3f9a4781aca
Tanya Ignatenko <tanigna.work@gmail.com>
2023-03-15 11:44:51 +0700

    Make our clients section responsive

--- css-practice/src/Landing/OurClientsSection/OurClientsSection.tsx
+++ css-practice/src/Landing/OurClientsSection/OurClientsSection.tsx
@@ -6,28 +6,38 @@ const OurClientsSection = () => {
     return (
         <div className={styles.contentWrapper}>
             <h3 className={styles.title}>Loved by big and small brands everywhere</h3>
-            <div className={styles.clientsBlock}>
-                <img
-                    src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/06/novasol.svg"
-                    className={styles.clientLogo}
-                />
-                <img
-                    src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/06/big-fish.svg"
-                    className={styles.clientLogo}
-                />
-                <img
-                    src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/06/marriott.svg"
-                    className={styles.clientLogo}
-                />
-                <img
-                    src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/06/new-york-times.svg"
-                    className={styles.clientLogo}
-                />
-                <img
-                    src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/06/electronic-arts.svg"
-                    className={styles.clientLogo}
-                />
-            </div>
+            <ul className={styles.clientsBlock}>
+                <li className={styles.clientLogoItem}>
+                    <img
+                        src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/06/novasol.svg"
+                        className={styles.clientLogo}
+                    />
+                </li>
+                <li className={styles.clientLogoItem}>
+                    <img
+                        src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/06/big-fish.svg"
+                        className={styles.clientLogo}
+                    />
+                </li>
+                <li className={styles.clientLogoItem}>
+                    <img
+                        src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/06/marriott.svg"
+                        className={styles.clientLogo}
+                    />
+                </li>
+                <li className={styles.clientLogoItem}>
+                    <img
+                        src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/06/new-york-times.svg"
+                        className={styles.clientLogo}
+                    />
+                </li>
+                <li className={styles.clientLogoItem}>
+                    <img
+                        src="https://docrdsfx76ssb.cloudfront.net/static/1678306332/pages/wp-content/uploads/2022/06/electronic-arts.svg"
+                        className={styles.clientLogo}
+                    />
+                </li>
+            </ul>
         </div>
     )
 };
